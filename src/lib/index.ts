2cbd15c87b2ff5d42bc92f965288100ca20c0192
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-05-15 14:39:25 +0200

    component exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,9 @@
-// Reexport your entry components here
+import Icons from "./Components/Icons/Icons.svelte"
+import LucideIcon from "./Components/Icons/LucideIcon.svelte";
+import SearchField from "./Components/SearchFields/SearchField.svelte";
+import SelectFieldWithSearch from "./Components/SearchFields/SearchField.svelte";
+
+export {Icons};
+export {SearchField};
+export {SelectFieldWithSearch};
+export {LucideIcon};

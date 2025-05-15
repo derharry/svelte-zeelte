f2792f37cc8d56c837ac940dd97684cfd7464270
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-05-15 08:38:56 +0200

    Init v0.0.1

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+
+	import '@picocss/pico';
+	import '../app.css';
+
+	import { base } from '$app/paths';
+
+	let { children } = $props();
+	
+</script>
+
+<nav>
+	<ul>
+		<li><a href="{base}/">Home</a></li>
+	</ul>
+</nav>
+
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Zeelte Lib</h1>
+<p>A collection of Svelte Components and JS or others that i re-use in projects with:</p>
+<ul>
+     <li>SvelteKit native  NodeJS-hosting.</li>
+     <li>SvelteKit static  Httpd-hosting (adapter-static, CSR, SSR with PHP).</li>
+     <li>native and static, SvelteKit + PHP, combined</li>
+</ul>
+
+
+<h3>Use PHP with SvelteKit (SvelteKit and PHP)</h3>
+<p>See ZeeltePHP for details - to use +page.server.php, +.server, +.php files in your SvelteKit project.</p>

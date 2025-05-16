593c939a6f4af7cbe8487644bb38eb53e2b99a23
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-05-16 16:55:41 +0200

    new styling to have option selected working

--- src/lib/Components/SearchFields/SelectFieldWithSearch.svelte
+++ src/lib/Components/SearchFields/SelectFieldWithSearch.svelte
@@ -16,7 +16,7 @@
 	export let name: string = '';
 
 	/** selected value for SELECT. id = index or id,uuid */
-	export let value = -1;
+	export let value = undefined;
 
 	/** size of SELECT (how many rows) */
 	export let size: number = 3;
@@ -33,6 +33,45 @@
 
 </script>
 
+<style>
+
+	/*
+		self styling because default selected is missing with skeletonUI 
+	*/
+	select {
+		margin-top: 1px;
+		width: 100%;
+		padding: 0.5rem 0rem;
+		font-size: 1em;
+		/*
+		border-radius: 0.5rem;
+		border: 1px solid #d1d5db; /* gray-300 * /
+		background: #fff;
+		color: #222;
+		font-size: 1rem;
+		transition: border-color 0.2s, box-shadow 0.2s;
+		*/
+	}
+	option {
+		padding: 0.25rem 0.75rem;
+	}
+	/*
+	option:selected {
+		background-color: red !important;
+	}
+	select:focus {
+		border-color: #f97316; /* orange-500 * /
+		box-shadow: 0 0 0 2px #fde68a88; /* subtle orange focus ring * /
+		outline: none;
+	}
+	select:checked,
+	select::selection
+	 {
+		background-color: DodgerBlue !important;
+	}
+	*/
+</style>
+
 <div class="mx-auto w-full preset-outlined-primary-500">
 
 	<SearchFieldNew 
@@ -41,33 +80,63 @@
 	/>
 
 	<slot>
-	<select  
-		bind:this={dom_select}
-		{name}
-		bind:value
-		{size} 
-		class="select rounded-container {cssClass}"
-		style="{cssStyle}"		
-		onchange={() => {
-        		// Update value_text with the selected option's label
-        		value_text = dom_select.options[dom_select.selectedIndex]?.textContent ?? '';
-    		}}
-	>
 
+		<select 
+			bind:this={dom_select}
+			bind:value
+			{name}
+			{size} 	
+			onchange={() => {
+				// Update value_text with the selected option's label
+				value_text = dom_select.options[dom_select.selectedIndex]?.textContent ?? '';
+				console.log('sel-change');
+			}}
+			class="{cssClass}"
+			style="{cssStyle}"
+		>
+		<!-- 
+			style="{cssStyle}
+		-->
+			{#each data_filtered as row (row?.id || row?.uuid || row)} <!-- Keyed each loop -->
+
+				{#if row instanceof Array}
+
+					<option value={row}>{row}</option>
+
+				{:else if row instanceof Object}
+
+					<option value={row?.id || row?.uuid || row}>
+						{#each Object.entries(row) as [key, val],id}
+							{#if !['id', 'uuid'].includes(key)}
+								{val}
+							{/if}
+						{/each}
+					</option>
+
+				{:else}
+
+					<option value={row}>{row}</option>
+
+				{/if}
+			{/each}
+		</select>
+
+
+		<!-- 
 		{#each data_filtered as row, id}
 
 			{#if row instanceof Array}
 
 				<!-- for now just dump,   future maybe styled? like tabbed  
 					<li>{row[1]} {row[2]} {row[3]}</li>
-				 -->
+				 -- >
 				<option value={row}>{row}</option>
 
 			{:else if row instanceof Object}
 
 				<!-- for now just dump,   future maybe styled? like tabbed  
 					<li>{row.name} {row.date} {row.anyvalue}</li>
-				 -->
+				 -- >
 				<option value={row?.id || row?.uuid || id}>
 					{#each Object.entries(row) as [key, value],ido}
 						{#if key != 'id' && key != 'uuid'} 
@@ -88,18 +157,17 @@
 				{#each Object.entries(data_filtered) as [id, attrs] (id)}
 					<option value={attrs?.id || '-1'}> {attrs?.name || '-'} </option>
 				{/each}
-				-->
+				-- >
 
 			{:else}
 
-				<!-- default fallback, just dump the content  -->
+				<!-- default fallback, just dump the content  -- >
 				<option value={row}>{row}</option>
 
 			{/if}
 
 		{/each}
-
-	</select>
+		-->
 	</slot>
 
 </div>

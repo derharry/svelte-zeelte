84d722f999f93474903286bc1825a6d6e1c6afcb
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-06-03 11:06:02 +0200

    update exposing components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,7 @@
-import Icons from "./Components/Icons/Icons.svelte"
-import LucideIcon from "./Components/Icons/LucideIcon.svelte";
-import SearchField from "./Components/SearchFields/SearchField.svelte";
-import SelectFieldWithSearch from "./Components/SearchFields/SearchField.svelte";
+export { default as Icons } from './Components/Icons/Icons.svelte';
+export { default as LucideIcon } from './Components/Icons/LucideIcon.svelte';
 
-export {Icons};
-export {SearchField};
-export {SelectFieldWithSearch};
-export {LucideIcon};
+export { default as SearchField } from './Components/SearchFields/SearchField.svelte';
+export { default as SelectFieldWithSearch } from './Components/SearchFields/SearchField.svelte';
+
+export { default as Marquee } from './Components/Loaders/Marquee.svelte';

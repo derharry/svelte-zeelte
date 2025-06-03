cfd03a76150f82c8491a75f7c34a1b199a14874d
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-06-03 11:05:01 +0200

    renamed Loader to Marquee

---
+++ src/lib/Components/Loaders/Marquee.svelte
@@ -0,0 +1,22 @@
+<script>
+     export let value = "?";
+     export let padding = "0px";
+     export let direction = "down";
+</script>
+
+<!-- svelte-ignore a11y_distracting_elements -->
+<marquee {direction} style="padding:{padding}">
+     <slot>
+          {@html value}
+     </slot>
+</marquee>
+
+<style>
+     marquee {
+          font-style: italic bold;
+          text-align: center;
+          display: inline-block;
+          width: fit-content;
+          height: fit-content;
+     }
+</style>
